5593223915969ec84f448089f9ee1b59c0ddd607
Ashutoshbot1 <ashutoshbot1@gmail.com>
2026-01-18 18:48:03 +0530

    fixed unknown conecpt

--- src/concepts/2_Union_Any_and_Unknown/3_unknown.ts
+++ src/concepts/2_Union_Any_and_Unknown/3_unknown.ts
@@ -1,5 +1,5 @@
 /*
- *unknown is a type that represents a value whose type is not yet known,
+ * unknown is a type that represents a value whose type is not yet known,
  * but must be checked before use.
  * The Line 'but must be checked before use' is very important.
  */
@@ -9,11 +9,11 @@ let marks: unknown;
 /*
  * Uncomment and hover over marks to see the error
  * marks.toUpperCase() is not permitted because the type of marks is unknown
- * Because of which we cannot use any methods on marks or perform any operations on it
- * Or perform any operations using it.
- * Reassigning a value to a variable of type unknown is not allowed
+ * Because of which we cannot use any methods or perform any operations using it.
  */
 
 // marks.toUpperCase();
-// const marks2: string = marks;
+// const marks2: unknown = marks + 1;
+// const marks2: number = marks + 1;
+
 console.log(marks);
